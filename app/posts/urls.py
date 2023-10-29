5de8d45a101c0e0b392b33419fb745854a6b1e43
ChrisOnsando <chrisnemwel@gmail.com>
2023-10-30 00:03:24 +0300

    feat(posts): users should be able to like,dislike and comment 	* users should be able to like a post 	* users should be able to dislike a post 	* users should be able to comment a post

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -3,9 +3,35 @@ from django.urls import path
 from app.posts.views import (
     PostListView, 
     PostListAllView,
+    PostDetailView,
+    PostCommentView,
+    PostCommentDetailView,
+    PostLikeView,
+    PostDislikeView,
 )
 
 urlpatterns = [
     path("post/", PostListView.as_view(), name="post-list"),
     path("posts/", PostListAllView.as_view(), name="all-posts"),
+    path(
+        "post/<str:post_id>/",
+        PostDetailView.as_view(),
+        name="post-detail",
+    ),
+    path("comments/", PostCommentView.as_view(), name="comment"),
+    path(
+        "comments/<str:post_id>/",
+        PostCommentDetailView.as_view(),
+        name="comment-delete",
+    ),
+     path(
+        "likes/<str:post_id>/like/",
+        PostLikeView.as_view(),
+        name="like",
+    ),
+    path(
+        "dislikes/<str:post_id>/dislike/",
+        PostDislikeView.as_view(),
+        name="dislike",
+    ),
 ]
--- app/posts/views.py
+++ app/posts/views.py
@@ -1,15 +1,23 @@
 from typing import Any
 
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 
-from app.posts.models import Post
+from app.posts.models import(
+    Post,
+    PostComment,
+) 
 from app.posts.permissions import IsOwnerOrReadOnly
 from app.posts.serializers import (
     PostSerializer,
+    PostCommentSerializer,
+    LikeSerializer,
+    DislikeSerializer,
 )
-
+from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
+from rest_framework.generics import UpdateAPIView
 
 class PostListView(generics.ListCreateAPIView):
     serializer_class = PostSerializer
@@ -24,3 +32,85 @@ class PostListAllView(generics.ListAPIView):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
 
+class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    lookup_field = "post_id"
+
+    def delete(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        """
+        Returns message on deletion of posts
+        """
+        self.destroy(request, *args, **kwargs)
+        return Response(
+            {"message": "Post deleted successfully"},
+            status=status.HTTP_204_NO_CONTENT,
+        )
+
+class PostLikeView(UpdateAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = LikeSerializer
+    queryset = Post.objects.all()
+    lookup_field = "post_id"
+    renderer_classes = (JSONRenderer,)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            response_data = {
+                "message": "Post liked successfully",
+            }
+
+            return Response(response_data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PostDislikeView(UpdateAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = DislikeSerializer
+    queryset = Post.objects.all()
+    lookup_field = "post_id"
+    renderer_classes = (JSONRenderer,)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            response_data = {
+                "message": "Post disliked successfully",
+            }
+
+            return Response(response_data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PostCommentView(generics.ListCreateAPIView):
+    serializer_class = PostCommentSerializer
+    queryset = PostComment.objects.all()
+    permission_classes = [IsAuthenticated]
+    renderer_classes = (JSONRenderer,)
+
+
+class PostCommentDetailView(generics.RetrieveDestroyAPIView):
+    serializer_class = PostCommentSerializer
+    queryset = PostComment.objects.all()
+    renderer_classes = (JSONRenderer,)
+    permission_classes = [IsAuthenticated]
+    lookup_field = "post_id"
+
+    def delete(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        """
+        Returns message on deletion of comments
+        """
+        self.destroy(request, *args, **kwargs)
+        return Response(
+            {"message": "Comment deleted successfully"},
+            status=status.HTTP_204_NO_CONTENT,
+        )
+    
